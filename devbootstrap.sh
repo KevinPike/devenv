6da6867d65b266148e2a081222bfb0091aa2c1b2
Kevin Pike <kevin.e.pike@gmail.com>
2014-10-01 23:51:42 -0400

    Adds docker

--- devbootstrap.sh
+++ devbootstrap.sh
@@ -8,5 +8,11 @@ homesick clone git://github.com/KevinPike/dotfiles.git
 homesick pull --all
 homesick symlink dotfiles
 
-# Utilities
+## Utilities
 apt-get install tmux
+
+# Docker
+apt-get -y install docker.io
+ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
+update-rc.d docker.io defaults
